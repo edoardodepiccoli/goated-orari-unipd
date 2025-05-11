ed201ea927e7ff29f21b848f025098e3dccadf79
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-11 17:16:49 +0200

    remove index action from CoursesController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,4 @@
 class CoursesController < ApplicationController
-  def index
-    @courses = Course.all
-    render json: @courses
-  end
-
   def sync
     Course.sync!
     render json: { message: "Courses synced successfully" }
