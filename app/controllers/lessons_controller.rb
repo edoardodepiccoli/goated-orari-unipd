2717d6e96f811ab0a005054ba983b72bfa2e9b7d
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-12 18:52:15 +0200

    refactor lessons index view and controller to group lessons by date
    - update LessonsController to group upcoming lessons by date for better organization
    - modify lessons index view to display lessons categorized under their respective dates
    - ensure proper handling of cases with no lessons found

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,11 +3,13 @@ class LessonsController < ApplicationController
 
   def index
     @courses = current_user.courses
-    @lessons = Lesson.where(course_id: @courses.pluck(:id))
+    lessons = Lesson.where(course_id: @courses.pluck(:id))
       .upcoming
       .order(date: :asc, start_time: :asc)
 
-    unless @lessons.any?
+    @lessons_by_date = lessons.group_by { |lesson| lesson.date }
+
+    unless lessons.any?
       redirect_to courses_path, alert: "Nessuna lezione trovata per i tuoi corsi. Meglio così AHAHAH!"
     end
   end
